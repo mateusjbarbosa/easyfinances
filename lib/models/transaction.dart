f065f4224fe98cabfcaf31e9f45a9b3326a0b638
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2023-10-17 20:49:41 -0300

    fix: change transaction id to optional

--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -1,17 +1,17 @@
 enum TransactionType { expense, income }
 
 class Transaction {
-  final String id;
+  final String? id;
   final String title;
   final TransactionType type;
   final double value;
   final DateTime date;
 
   Transaction({
-    required this.id,
     required this.title,
     required this.type,
     required this.value,
     required this.date,
+    this.id,
   });
 }
