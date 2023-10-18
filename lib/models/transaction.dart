fa575e38fc5730c4f3ff42c9ff4fb235e7f2385b
Mateus José Barbosa <dev.mateusbarbosa@gmail.com>
2023-10-18 15:06:03 -0300

    feat: create toMap function in transaction model

--- lib/models/transaction.dart
+++ lib/models/transaction.dart
@@ -1,17 +1,32 @@
 enum TransactionType { expense, income }
 
 class Transaction {
-  final String? id;
-  final String title;
+  final int? id;
+  final String description;
   final TransactionType type;
   final double value;
   final DateTime date;
 
   Transaction({
-    required this.title,
+    required this.description,
     required this.type,
     required this.value,
     required this.date,
     this.id,
   });
+
+  Map<String, Object> toMap() {
+    Map<String, Object> obj = {
+      'description': description,
+      'type': type.toString(),
+      'value': value,
+      'date': date.toIso8601String(),
+    };
+
+    if (id != null) {
+      obj['id'] = id as int;
+    }
+
+    return obj;
+  }
 }
